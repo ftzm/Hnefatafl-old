ca191f8a5fdaf2883965dc83c651cc25e5152e85
Matthew Fitzsimmons <fitz.matt.d@gmail.com>
2019-02-10 17:14:19 -0500

    board improvements

--- src/Board.hs
+++ src/Board.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FlexibleInstances  #-}
 {-# LANGUAGE TupleSections  #-}
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE DeriveGeneric  #-}
 
 module Board
   (Piece
@@ -30,6 +33,7 @@ module Board
   ,blackPiece
   ,opp
   )
+
 where
 
 import Data.List
@@ -37,9 +41,10 @@ import Data.Int
 import GHC.Generics
 import Data.Maybe
 import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed.Deriving
 
-data Piece = White | Black | King | Empty | Corner
-  deriving (Show, Eq, Generic)
+-------------------------------------------------------------------------------
+-- Direction
 
 data Direction = North | South | East | West
   deriving (Show, Eq, Ord)
@@ -50,45 +55,17 @@ opp South = North
 opp East  = West
 opp West  = East
 
-type Coord = (Int, Int)
-type Square = (Coord,Piece)
+-------------------------------------------------------------------------------
+-- Piece
 
-boardSize :: Int
-boardSize = 11
+data Piece = White | Black | King | Empty | Corner
+  deriving (Show, Eq, Generic)
 
-xyToInt :: Coord -> Int
-xyToInt (x,y) = boardSize * y + x
-
-intToXY :: Int -> Coord
-intToXY i = (\(y,x) -> (x,y)) $ divMod i 11
-
-blackStart :: [Coord]
-blackStart = [(3,0),(4,0),(5,0),(6,0),(7,0),(5,1),(0,3),(10,3),(0,4),(10,4),
-              (0,5),(1,5),(9,5),(10,5),(0,6),(10,6),(0,7),(10,7),(5,9),(3,10),
-              (4,10),(5,10),(6,10),(7,10)]
-
-whiteStart :: [Coord]
-whiteStart = [(5,3),(4,4),(5,4),(6,4),(3,5),(4,5),(6,5),(7,5),(4,6),(5,6),
-              (6,6),(5,7)]
-
-cornerCoords :: [Coord]
-cornerCoords = [(0,0),(10,0),(0,10),(10,10)]
-
-throne :: Coord
-throne = (5,5)
-
-type Board = V.Vector Int8
-
-emptyBoard :: Board
-emptyBoard = V.fromList $ replicate 121 0
-
-startBoard :: Board
-startBoard = foldl' putPieceBatch emptyBoard [bs, ws, cs, k]
-  where
-    bs = zip blackStart (repeat Black)
-    ws = zip whiteStart (repeat White)
-    cs = zip cornerCoords (repeat Corner)
-    k = [(throne,King)]
+-- Use some th magic so make the Piece adt unboxed for Vector
+derivingUnbox "Piece"
+    [t| Piece -> Int8 |]
+    [| pieceToInt8 |]
+    [| int8ToPiece |]
 
 int8ToPiece :: Int8 -> Piece
 int8ToPiece i
@@ -108,16 +85,63 @@ pieceToInt8 p
   | p == King   = 4
 pieceToInt8 _ = 0
 
+-------------------------------------------------------------------------------
+
+type Coord = (Int, Int)
+type Square = (Coord,Piece)
+type Board = V.Vector Piece
+
+-------------------------------------------------------------------------------
+-- Important Coords
+
+boardSize :: Int
+boardSize = 11
+
+blackStart :: [Coord]
+blackStart = [(3,0),(4,0),(5,0),(6,0),(7,0),(5,1),(0,3),(10,3),(0,4),(10,4),
+              (0,5),(1,5),(9,5),(10,5),(0,6),(10,6),(0,7),(10,7),(5,9),(3,10),
+              (4,10),(5,10),(6,10),(7,10)]
+
+
+whiteStart :: [Coord]
+whiteStart = [(5,3),(4,4),(5,4),(6,4),(3,5),(4,5),(6,5),(7,5),(4,6),(5,6),
+              (6,6),(5,7)]
+
+cornerCoords :: [Coord]
+cornerCoords = [(0,0),(10,0),(0,10),(10,10)]
+
+throne :: Coord
+throne = (5,5)
+
+-------------------------------------------------------------------------------
+
+xyToInt :: Coord -> Int
+xyToInt (x,y) = boardSize * y + x
+
+intToXY :: Int -> Coord
+intToXY i = (\(y,x) -> (x,y)) $ divMod i 11
+
+emptyBoard :: Board
+emptyBoard = V.fromList $ replicate 121 Empty
+
+startBoard :: Board
+startBoard = foldl' putPieceBatch emptyBoard [bs, ws, cs, k]
+  where
+    bs = zip blackStart (repeat Black)
+    ws = zip whiteStart (repeat White)
+    cs = zip cornerCoords (repeat Corner)
+    k = [(throne,King)]
+
 getPiece :: Board -> Coord -> Piece
-getPiece b c = int8ToPiece $ b V.! xyToInt c
+getPiece b c = b V.! xyToInt c
 
 putPiece :: Coord -> Piece -> Board -> Board
 putPiece c p b = putPieceBatch b [(c,p)]
 
-putPieceBatch :: Board -> [Square] ->  Board
+putPieceBatch :: Board -> [Square] -> Board
 putPieceBatch b ss = b V.// map convert ss
   where
-    convert (c, p) = (xyToInt c, pieceToInt8 p)
+    convert (c, p) = (xyToInt c, p)
 
 deletePiece :: Square -> Board -> Board
 deletePiece (c,_) = putPiece c Empty
@@ -138,13 +162,13 @@ maybeCoord d (x,y)
   | South <- d = (x,) . (+1)       <$> ifMaybe y (10>)
   | East  <- d = (,y) . (+1)       <$> ifMaybe x (10>)
   | West  <- d = (,y) . subtract 1 <$> ifMaybe x (0<)
-maybeCoord _ _ = Nothing
 
 go :: Board -> Square -> Direction -> Maybe Square
 go b (c,_) d = getSquare b <$> maybeCoord d c
 
 toEdge :: Square -> Direction -> [Coord]
-toEdge (c,_) d = map fromJust $ takeWhile isJust $ iterate (maybeCoord d =<<) $ maybeCoord d c
+toEdge (c,_) d = map fromJust $ takeWhile isJust
+               $ iterate (maybeCoord d =<<) $ maybeCoord d c
 
 toEdge' :: Square -> Direction -> [Coord]
 toEdge' ((x,y),_) d
